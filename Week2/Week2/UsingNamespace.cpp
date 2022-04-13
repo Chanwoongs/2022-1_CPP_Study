4df6ddc065c130155885d5f8cac1cdd503e72770
Chanwoongs <tkfkdcksdnd@daum.net>
2022-04-14 00:44:31 +0900

    Namespace Examples

---
+++ Week2/Week2/UsingNamespace.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // input output stream <- cout과 << 연산자 포함
+using namespace std;
+
+int main()
+{
+	cout << "Hello C++\n"; // Hello C++를 출력하고 다음 줄로 넘어감
+	cout << "첫 C++ 예제입니다." << std::endl;
+
+	return 0; // 0을 반환하고, 프로그램 종료
+}

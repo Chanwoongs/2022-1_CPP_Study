bcc39362341f16c5193080e64fce3a1853a546d5
Chanwoongs <tkfkdcksdnd@daum.net>
2022-04-14 00:44:05 +0900

    Basic C++ Input Examples

---
+++ Week2/Week2/Input.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num = 0;
+
+	cout << "숫자 입력 : ";
+	
+	cin >> num;
+
+	cout << "입력된 숫자 : " << num << endl; // %d 사용할 필요 없이 변수를 쓰면 된다.
+}
+

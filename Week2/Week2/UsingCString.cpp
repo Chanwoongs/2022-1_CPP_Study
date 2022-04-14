69b1b996138fb970aa46199843e6e7ef4333b458
Chanwoongs <tkfkdcksdnd@daum.net>
2022-04-14 21:49:36 +0900

    C string을 이용한 문자열 입력

--- Week2/Week2/UsingCString.cpp
+++ Week2/Week2/UsingCString.cpp
@@ -8,12 +8,14 @@ int main()
 	cout << "제 이름은 " << name << " 입니다." << endl;
 
 	char email[100] = {};
+	cout << "이메일 입력하세요 : ";
 	cin >> email;
 	cout << "이메일 : " << email << endl;
 
 	cin.ignore(100, '\n'); // 입력 버퍼 비워주기
 
-	cin.getline(email, 100, '\n');
+	cout << "이메일 입력하세요 : ";
+	cin.getline(email, 100, '\n'); // 엔터 키를 누를때 까지
 	cout << "이메일 : " << email << endl;
 }
 
